83c83ca8c57cba83bf6fe396a7516cd9872a2158
AugustExner <exneraugust@gmail.com>
2024-05-06 10:31:57 +0200

    Latest

--- CordulusPrivate/app/cordulusApi/apiUpdateApp.js
+++ CordulusPrivate/app/cordulusApi/apiUpdateApp.js
@@ -7,17 +7,26 @@ import {
   FlatList,
   View,
   StatusBar,
+  Image,
 } from "react-native";
 
 import { readSensorArray } from "../sensorStorage";
 
 import { Link, router } from "expo-router";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useId } from "react";
 
 export default function updateAppPost() {
   const [refreshing, setRefreshing] = useState(false);
   const [postData, setPostData] = useState(null); // State to store API response
 
+  generateRandomNumber = () => {
+    const min = 1;
+    const max = 10000;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
+  };
+
+  let id = 0;
   const fetchData = async () => {
     const data = await readSensorArray();
     if (data) {
@@ -38,43 +47,37 @@ export default function updateAppPost() {
     console.log("PostData--->", postData);
   }, [postData]);
 
+  const renderItem = ({ item }) => (
+    <View style={styles.postContainer}>
+      <Text style={styles.sensorText}>Plants: {item.plants}</Text>
+      <Text style = {styles.regularText}> SensorID: {item.id}</Text>
+      <Text style = {styles.regularText}> Temperature: {item.temperature}</Text>
+      <Text style = {styles.regularText}> Humidity: {item.humidity}</Text>
+      <Text style = {styles.regularText}> Moisture: {item.moisture}</Text>
+      <Image
+          style={styles.image}
+          source={require("../../assets/gardenBed.png")}
+        />
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="lightgrey" />
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
+      <FlatList
+        data={postData}
+        renderItem={renderItem}
+        keyExtractor={generateRandomNumber} // Unique key for each item
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-      >
-        <Text>Pull down to see RefreshControl indicator</Text>
-        {postData && renderPostData(postData)}
-      </ScrollView>
+      />
     </SafeAreaView>
   );
 }
 
-const renderPostData = (data) => {
-  return (
-    <View>
-      {data.map((item, index) => (
-        <View key={index} style={styles.postContainer}>
-          <Text style={styles.sensorText}>Plants: {item.plants}</Text>
-          <Text>SensorID: {item.id}</Text>
-          <Text>Temperature: {item.temperature}</Text>
-          <Text>Humidity: {item.humidity}</Text>
-          <Text>Moisture: {item.moisture}</Text>
-        </View>
-      ))}
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  scrollView: {
     flex: 1,
     backgroundColor: "lightgrey",
     alignItems: "center",
@@ -82,10 +85,10 @@ const styles = StyleSheet.create({
   postContainer: {
     backgroundColor: "white",
     padding: 10,
-    width: 300,
+    width: 380,
     borderRadius: 8,
     borderWidth: 2,
-    marginBottom: 16, // Add margin between cards if needed
+    marginBottom: 18, // Add margin between cards if needed
     shadowColor: "black",
     shadowOffset: {
       width: 0,
@@ -95,9 +98,23 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+
   sensorText: {
-    
     marginBottom: 5,
     fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    fontSize: 18,
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+
+  regularText: {
+    marginBottom: 8, 
   }
 });
